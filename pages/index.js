d843a570c8fb419268ee8d73c2bcaef7f4e4ca71
okkdev <stehlik.jen@gmail.com>
2021-07-20 21:45:08 +0200

    add validation and flash

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,77 @@ import { useState } from "react"
 
 export default function Home() {
   const [lobby, setLobby] = useState("")
+  const [flash, setFlash] = useState({
+    error: false,
+    message: "",
+  })
+
+  async function validateLobby() {
+    if (!/^[0-9]{4}$/.test(lobby)) {
+      setFlash({
+        error: true,
+        message: lobby + " is not a valid Lobby",
+      })
+    } else {
+      const res = await fetch(
+        "https://concerto-mbaacc.herokuapp.com/s?action=check&id=" + lobby
+      )
+      const data = await res.json()
+
+      if (data.status !== "OK") {
+        setFlash({
+          error: true,
+          message: "Lobby #" + lobby + " does not exist",
+        })
+      }
+    }
+  }
 
   function copyLink() {
     const cb = navigator.clipboard
     cb.writeText("invite.meltyblood.club/" + lobby).then(() =>
-      alert("Text copied")
+      setFlash({
+        error: false,
+        message: "Invite Link copied to clipboard",
+      })
     )
+  }
+
+  async function copyClick() {
+    await validateLobby()
+    if (!flash.error) {
+      copyLink()
+    }
   }
 
   return (
     <div className="px-6 mt-5 text-xl font-medium tracking-wider text-center bg-gray-900 rounded-lg shadow-lg py-9">
-      <div className="grid grid-cols-2 grid-rows-2 gap-3">
+      <div className="grid grid-cols-2 gap-3">
         <input
           value={lobby}
           onChange={(e) => setLobby(e.target.value)}
           type="text"
           pattern="^[0-9]{4}$"
           maxLength="4"
-          class="shadow-sm focus:border-2 outline-none px-3 focus:outline-none block w-full border-gray-100 border bg-gray-900 rounded-md"
+          className="block w-full px-3 bg-gray-900 border border-gray-100 rounded-md shadow-sm outline-none focus:border-2 focus:outline-none"
           placeholder="Lobby Code"
           required
         />
         <button
-          onClick={copyLink}
-          className="inline-flex items-center px-6 py-3 text-base font-medium text-center bg-red-700 border border-transparent rounded-md shadow-sm hover:bg-red-800 focus:outline-none"
+          onClick={copyClick}
+          className="inline-flex items-center px-6 py-3 text-base font-medium bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-purple-800 focus:outline-none"
         >
           Copy Invite Link
         </button>
-        {/* TODO: Alert Validation stuff <div className="col-span-2 bg-red-300 empty:hidden"></div> */}
+        <div
+          className={
+            flash.error
+              ? "bg-red-400 border-red-500 col-span-2 p-4 mt-5 border rounded-md empty:hidden"
+              : "bg-green-400 border-green-500 col-span-2 p-4 mt-5 border rounded-md empty:hidden"
+          }
+        >
+          {flash.message}
+        </div>
       </div>
     </div>
   )
