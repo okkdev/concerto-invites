40d0a8837a3e1d74564568d42b355acfbbea2dc7
okkdev <stehlik.jen@gmail.com>
2021-07-20 22:49:42 +0200

    links to resources

--- pages/_app.js
+++ pages/_app.js
@@ -7,13 +7,26 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Concerto Invites</title>
-        <link rel="icon" href="/img/concerto_icon.png" />
       </Head>
       <div className="max-w-xl pt-2 mx-auto">
         <div className="text-2xl font-bold tracking-widest">
           Concerto Invites
         </div>
         <Component {...pageProps} />
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <a
+            href="https://concerto.shib.live"
+            className="py-4 text-sm font-medium tracking-wider text-center bg-gray-900 bg-opacity-75 rounded-lg shadow-lg hover:bg-opacity-100"
+          >
+            Get Concerto
+          </a>
+          <a
+            href="https://play.meltyblood.club"
+            className="py-4 text-sm font-medium tracking-wider text-center bg-gray-900 bg-opacity-75 rounded-lg shadow-lg hover:bg-opacity-100"
+          >
+            Get Melty Blood
+          </a>
+        </div>
       </div>
     </>
   )
