2a883f3214835ae6c2f6a570e42945527f27f341
Henning Dahlheim <hactar@cyberkraft.ch>
2021-07-23 12:11:36 +0200

    Refactor lobby page to use more components

--- pages/[lobby].js
+++ pages/[lobby].js
@@ -2,96 +2,106 @@ import Head from "next/head"
 import { useEffect } from "react"
 
 const Lobby = ({ valid, data }) => {
-  let body = <>Something went wrong</>
-
-  if (valid) {
-    if (data.open) {
-      const msg = "Invite to " + data.type + " Lobby #" + data.lobby
-      body = (
-        <>
-          <Head>
-            <meta property="og:title" content={msg} />
-            <meta name="theme-color" content="#3ca864" />
-            <meta
-              property="og:description"
-              content="Click the link to join the lobby with Concerto!"
-            />
-          </Head>
-          <div>{msg}</div>
-          <a
-            href={"concerto://lobby:" + data.lobby}
-            className="inline-flex items-center px-6 py-3 mt-3 text-base font-medium bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-purple-800 focus:outline-none"
-          >
-            Click here if Concerto didn't launch automatically
-          </a>
-          <div className="mt-6">
-            <p className="text-sm">Still not working?</p>
-            <p className="text-xs">
-              Did you run Concerto as administrator once to register the
-              handler?
-            </p>
-          </div>
-        </>
-      )
-      useEffect(() => {
-        window.location.replace("concerto://lobby:" + data.lobby)
-      }, [])
-    } else {
-      const msg = "Lobby #" + data.lobby + " does not exist"
-      body = (
-        <>
-          <Head>
-            <meta property="og:title" content={msg} />
-            <meta name="theme-color" content="#ad3232" />
-            <meta
-              property="og:description"
-              content="Create a new lobby in Concerto!"
-            />
-          </Head>
-          <div>{msg}</div>
-        </>
-      )
-    }
-  } else {
-    const msg = data.lobby + " is not a valid lobby"
-    body = (
-      <>
-        <Head>
-          <meta property="og:title" content={msg} />
-          <meta name="theme-color" content="#ad3232" />
-          <meta
-            property="og:description"
-            content="Create a new lobby in Concerto!"
-          />
-        </Head>
-        <div>{msg}</div>
-      </>
-    )
+  if (!valid) {
+    return <InvalidLobby host={data.host} lobby={data.lobby} />
   }
 
-  return (
-    <>
-      <Head>
-        <title>Lobby Invite #{data.lobby}</title>
-        <meta
-          property="og:image"
-          content={"https://" + data.host + "/img/concerto_icon.png"}
-        />
-      </Head>
+  if (!data.open) {
+    return <LobbyDoesNotExist host={data.host} lobby={data.lobby} />
+  }
 
-      <div className="px-6 mt-5 text-xl font-medium tracking-wider text-center bg-gray-900 rounded-lg shadow-lg py-9">
-        {body}
+  return <OpenLobby host={data.host} lobby={data.lobby} type={data.type}/>
+}
+
+function LobbyDoesNotExist({host, lobby}) {
+  const msg = `Lobby #${lobby} does not exist`
+  return <LobbyLayout lobby={lobby} host={host}>
+    <Head>
+      <meta property="og:title" content={msg} />
+      <meta name="theme-color" content="#ad3232" />
+      <meta
+        property="og:description"
+        content="Create a new lobby in Concerto!" />
+    </Head>
+    <p>{msg}</p>
+  </LobbyLayout>
+}
+
+function OpenLobby({host, lobby, type}) {
+  const msg = `Invite to ${type} Lobby #${lobby}`
+
+  useEffect(() => {
+    window.location.replace(`concerto://lobby:${lobby}`)
+  }, [])
+
+  return <LobbyLayout host={host} lobby={lobby}>
+      <Head>
+        <meta property="og:title" content={msg} />
+        <meta name="theme-color" content="#3ca864" />
+        <meta
+          property="og:description"
+          content="Click the link to join the lobby with Concerto!" />
+      </Head>
+      <p>{msg}</p>
+      <a
+        href={`concerto://lobby:${lobby}`}
+        className="inline-flex items-center px-6 py-3 mt-3 text-base font-medium bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-purple-800 focus:outline-none"
+      >
+        Click here if Concerto didn't launch automatically
+      </a>
+      <div className="mt-6">
+        <p className="text-sm">Still not working?</p>
+        <p className="text-xs">
+          Did you run Concerto as administrator once to register the
+          handler?
+        </p>
       </div>
-    </>
-  )
+    </LobbyLayout>
+}
+
+function InvalidLobby({host, lobby}) {
+  const msg = `${lobby} is not a valid lobby`
+
+  return <LobbyLayout host={host} lobby={lobby}>
+    <Head>
+      <meta property="og:title" content={msg} />
+      <meta name="theme-color" content="#ad3232" />
+      <meta
+        property="og:description"
+        content="Create a new lobby in Concerto!" />
+    </Head>
+    <p>{msg}</p>
+  </LobbyLayout>
+}
+
+function LobbyLayout({host, lobby, children}) {
+  return <>
+    <Head>
+      <title>Lobby Invite #{lobby}</title>
+      <meta
+        property="og:image"
+        content={`https://${host}/img/concerto_icon.png`} />
+    </Head>
+
+    <div className="px-6 mt-5 text-xl font-medium tracking-wider text-center bg-gray-900 rounded-lg shadow-lg py-9">
+      {children}
+    </div>
+  </>
 }
 
 export async function getServerSideProps(context) {
-  console.log(context)
   const lobby = context.params.lobby
   const valid = /^[0-9]{4}$/.test(lobby)
-  let open = false
-  let type = ""
+
+  const props = {
+    valid: valid,
+    data: {
+      host: context.req.headers.host,
+      lobby: lobby,
+      open: false,
+      type: "",
+    }
+  }
 
   if (valid) {
     const res = await fetch(
@@ -100,19 +110,9 @@ export async function getServerSideProps(context) {
     const data = await res.json()
 
     if (data.status === "OK") {
-      open = true
-      type = data.type
+      props.data.open = true
+      props.data.type = data.type
     }
-  }
-
-  const props = {
-    valid: valid,
-    data: {
-      host: context.req.headers.host,
-      lobby: lobby,
-      open: open,
-      type: type,
-    },
   }
 
   return {
